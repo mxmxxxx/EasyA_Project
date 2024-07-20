3af425c10be5a38ae2e27a18cfb3ec87345cd647
mxmxxxx <minghan.xiao@outlook.com>
2024-07-21 00:37:27 +0100

    bug fixed

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import WalletConnect from './WalletConnect';
 
 const HeaderContainer = styled.header`
   display: flex;
